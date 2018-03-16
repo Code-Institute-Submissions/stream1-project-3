f42f3c89c2bff94d143d05b4ce2e98e8023bf021
saraa123 <address>
2018-03-16 10:06:50 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+	$("h1").mouseenter(function() {
+		$("h1").addClass("h1_color");
+	});
+
+	$("h1").mouseleave(function() {
+		$("h1").removeClass("h1_color");
+
+	});
+
+	
+});
+
